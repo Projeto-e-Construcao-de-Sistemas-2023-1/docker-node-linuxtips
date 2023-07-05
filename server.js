f604675a77a644be1735b78e7a2d85247c624f17
Rômulo Pastore <88741244+Romumoon@users.noreply.github.com>
2023-07-05 19:15:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Jojo e Romumu do DevOps!');
+  res.send('Jojo gaymer e Romumu ruindade pura do DevOps!');
 });
 
 app.get('*', (req, res) => {
